9678a24e8198b51dcc62f6b838355c81232990dc
Milos Popovic <misa01523@gmail.com>
2025-06-10 11:02:18 +0200

    Navbar dropdown hover focus, contrasts

--- webapp/src/main/webapp/js/languageMenuUtils.js
+++ webapp/src/main/webapp/js/languageMenuUtils.js
@@ -5,22 +5,40 @@ $(function() {
 	$.extend(this, i18nStringsLangMenu);
 	var theText = this.selectLanguage;
 	var imgHTML = "";
-    $("ul.language-dropdown li#language-menu").hover(function(){
+
+	function showMenu() {
 		$("a#lang-link").html(theText);
-        $(this).addClass("hover");
-        $('ul:first',this).css('visibility', 'visible');
-        $("ul.language-dropdown ul.sub_menu li").css('width', ($("ul.language-dropdown").width() - 4) + 'px');
+		$(this).addClass("hover");
+		$('ul:first', this)
+			.css('visibility', 'visible')
+			.attr('aria-hidden', 'false');
+		$(this).attr('aria-expanded', 'true');
+		$("ul.language-dropdown ul.sub_menu li").css('width', ($("ul.language-dropdown").width() - 4) + 'px');
+	}
 
-    }, function(){
-    	$("a#lang-link").html(imgHTML);
-        $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
-    });
+	function hideMenu() {
+		$("a#lang-link").html(imgHTML);
+		$(this).removeClass("hover");
+		$('ul:first', this)
+			.css('visibility', 'hidden')
+			.attr('aria-hidden', 'true');
+		$(this).attr('aria-expanded', 'false');
+	}
 
-    $("ul.language-dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+	$("ul.language-dropdown li#language-menu")
+		.hover(showMenu, hideMenu)
+		.on('focusin', showMenu)
+		.on('focusout', function(e) {
+			var relatedTarget = e.relatedTarget;
+			if (!$.contains(this, relatedTarget)) {
+				hideMenu.call(this);
+			}
+		});
+
+	$("ul.language-dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
 
 	$("ul.language-dropdown ul.sub_menu li").each(function() {
-		if ( $(this).attr("status") == "selected" ) {
+		if ($(this).attr("status") == "selected") {
 			$("a#lang-link").html($(this).children("a").html());
 			imgHTML = $(this).children("a").html();
 		}
--- webapp/src/main/webapp/js/userMenu/userMenuUtils.js
+++ webapp/src/main/webapp/js/userMenu/userMenuUtils.js
@@ -2,20 +2,38 @@
 
 $(function(){
 
-    $("ul.dropdown li").hover(function(){
+    function showMenu() {
         $(this).addClass("hover");
-        $('ul:first',this).css('visibility', 'visible');
-        if ( $('ul.dropdown').width() > 88 ) {
-            $('ul:first li',this).css('width', ($("ul.dropdown").width() - 22) + 'px');
+        $('ul:first', this)
+            .css('visibility', 'visible')
+            .attr('aria-hidden', 'false');
+        $(this)
+            .attr('aria-expanded', 'true');
+        if ($('ul.dropdown').width() > 88) {
+            $('ul:first li', this).css('width', ($("ul.dropdown").width() - 22) + 'px');
         }
         $("ul.dropdown ul.sub_menu li:last-child").css('width', ($("ul.dropdown").width() - 14) + 'px');
+    }
 
-    }, function(){
-
+    function hideMenu() {
         $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
+        $('ul:first', this)
+            .css('visibility', 'hidden')
+            .attr('aria-hidden', 'true');
+        $(this)
+            .attr('aria-expanded', 'false');
+    }
 
-    });
+    $("ul.dropdown li")
+        .hover(showMenu, hideMenu)
+        .on('focusin', showMenu)
+        .on('focusout', function(e) {
+            // Check if the newly focused element is still inside this li
+            var relatedTarget = e.relatedTarget;
+            if (!$.contains(this, relatedTarget)) {
+                hideMenu.call(this);
+            }
+        });
 
     $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
 
